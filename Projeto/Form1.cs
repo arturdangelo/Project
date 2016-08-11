78b8f03813275082f7216d2bdbbe488970c64c40
artur <arturdangelo@gmail.com>
2016-08-10 21:59:49 -0300

    Projeto

--- Projeto/Form1.cs
+++ Projeto/Form1.cs
@@ -6,14 +6,38 @@ using System.Drawing;
 using System.Linq;
 using System.Text;
 using System.Windows.Forms;
+using MySql.Data.MySqlClient;
 
 namespace Projeto
 {
     public partial class Form1 : Form
     {
+        private MySqlConnection bdConn; //Conexão com o banco de dados
+        private DataSet bdDataSet; //MySQL
+
         public Form1()
         {
+
             InitializeComponent();
+            this.Height = Screen.PrimaryScreen.Bounds.Height;
+            this.Width = Screen.PrimaryScreen.Bounds.Width;
+            this.TopMost = true;
+
+            //Definição do dataset
+            bdDataSet = new DataSet();
+            //Define string de conexão
+            bdConn = new MySqlConnection("Persist Security Info=False;server=localhost;database=bdSystem;uid=root;pwd=''");
+
+            //Abre conexão
+            try
+            {
+                bdConn.Open();
+                MessageBox.Show("Conexão aberta");
+            }
+            catch
+            {
+                MessageBox.Show("Impossível estabelecer conexão");
+            }
         }
 
         private void button1_Click(object sender, EventArgs e)
